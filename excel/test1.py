27e6ed1a0aa0fb2101ed0ff20f4dc5bb951ec8ef
shiliang <shiliang8823@163.com>
2017-12-19 19:42:31 +0800

    new code

--- excel/test1.py
+++ excel/test1.py
@@ -9,19 +9,18 @@ import sys
 import xlrd
 
 data = xlrd.open_workbook('output.xls')
-
-table = data.sheets()[0]
-table2= data.sheet_by_name(u'导出工作表')
-
+table= data.sheet_by_name(u'导出工作表')
 nrows_num = table.nrows
-
 ncols_num = table.ncols
-
-aa= table2.nrows
-
 res = []
-aa=table.cell(1,0).value
-print aa
+fileName='盾安控股集团有限公司.docx'
+for i in range(1,nrows_num):
+    aa=str(table.cell(i,0).value).decode("unicode_escape").encode("utf8")
+    if aa in fileName:
+        print 'yicunzai'
+    else:
+        i=i+1
+
 # for i in range(nrows_num):
 #         print str(table.row_values(i)).decode("unicode_escape").encode("utf8")
 
