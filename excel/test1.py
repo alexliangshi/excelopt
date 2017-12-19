c55b59db283f639ea7c7d8be4fa93a38e8b413c4
shiliang <shiliang8823@163.com>
2017-12-19 19:32:41 +0800

    new code

--- excel/test1.py
+++ excel/test1.py
@@ -4,11 +4,24 @@
 # @Contact : shiliang@chinaratings.com.cn
 # @Site    :
 
+# coding=utf-8
+import sys
 import xlrd
-#import chardet
-data = xlrd.open_workbook("output.xls")
+
+data = xlrd.open_workbook('output.xls')
+
 table = data.sheets()[0]
-nrows = table.nrows
-for i in range(nrows):
-#print chardet.detect(str(table.row_values(i)))
-        print str(table.row_values(i)).decode("unicode_escape").encode("utf8")
+table2= data.sheet_by_name(u'导出工作表')
+
+nrows_num = table.nrows
+
+ncols_num = table.ncols
+
+aa= table2.nrows
+
+res = []
+aa=table.cell(1,0).value
+print aa
+# for i in range(nrows_num):
+#         print str(table.row_values(i)).decode("unicode_escape").encode("utf8")
+
